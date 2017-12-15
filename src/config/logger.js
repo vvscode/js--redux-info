36e44be6ead7e548e1fd7a51f232722cbf3899dd
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-12-16 00:23:37 +0300

    Add custom logic via logger

--- src/config/logger.js
+++ src/config/logger.js
@@ -2,6 +2,10 @@
 // https://redux.js.org/docs/api/applyMiddleware.html
 const logger = ({ /* dispatch, */ getState }) => next => action => {
   console.info("Custom logger:", action.type, action.payload);
+
+  if (action.type === "TOGGLE_CHECK" && action.payload % 2 === 0) {
+    return; // do not allow toggle check 
+  }
   next(action);
 };
 
